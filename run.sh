afc417562fbf5e22511b2c6f837a613be8d08d40
Johannes Innerbichler <johannes.innerbichler@salzburgresearch.at>
2017-05-31 13:58:57 +0200

    added utils to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,27 @@
 #!/usr/bin/env bash
 
-# build maven project
-mvn clean install -DskipTests
+if [ "$1" == "run" ]; then
+    # build maven project
+    mvn clean install -DskipTests
 
-# run docker compose
-docker-compose up
+    # run docker compose
+    docker-compose up
+
+elif [ "$1" == "bx-deploy" ]; then
+
+    # create local docker image
+    docker build -t nimbleplatform/marmotta-backend --build-arg DB_HOST=marmotta-db --build-arg DB_PORT=5432 --build-arg DB_USER=root --build-arg DB_PASS=changeme .
+
+    # push to docker hub
+    docker push nimbleplatform/marmotta-backend
+
+    # add to Bluemix container registry
+    bx ic cpi nimbleplatform/marmotta-backend registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
+
+    # run postgres container
+    bx ic run --name marmotta-db -e "POSTGRES_PASSWORD=changeme" -e "POSTGRES_USER=root" -e "POSTGRES_DB=marmotta" -p 5432:5432  -m 256 registry.eu-gb.bluemix.net/semantic_mediator_container/postgre
+
+    # run marmotta
+    bx ic run --name marmotta-backend --link marmotta-db:marmotta-db -p 8080:8080 -m 1024 registry.eu-gb.bluemix.net/semantic_mediator_container/marmotta-backend
+
+fi
